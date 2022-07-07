e81ee65ea179018369aa3991588d85ebf99abdbf
parvind.sharma <parvind.sharma@investisdigital.com>
2022-07-07 12:44:31 +0530

    AAP News

--- JS/scripts.js
+++ JS/scripts.js
@@ -299,23 +299,23 @@ $(document).ready(function(){
 						searchResultsHTML += '<div class="modal fade" id="exampleModal' + i +'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">';
 							searchResultsHTML += '<div class="modal-dialog" role="document">';
 								searchResultsHTML += '<div class="modal-content col-sm-12 col-lg-12">';
-									searchResultsHTML += '<div class="col-sm-6 moviePosterInModal">';
-										searchResultsHTML += '<a href="'+youtubeLink+'"><img src="'+poster+'"></a>'; 
-									searchResultsHTML += '</div><br>';//close trailerLink
-									searchResultsHTML += '<div class="col-sm-6 movieDetails">';
+									// searchResultsHTML += '<div class="col-sm-6 moviePosterInModal">';
+									// 	searchResultsHTML += '<a href="'+youtubeLink+'"><img src="'+poster+'"></a>'; 
+									// searchResultsHTML += '</div><br>';//close trailerLink
+									searchResultsHTML += '<div class="col-sm-12 movieDetails">';
 										searchResultsHTML += '<div class="movieName">'+title+'</div><br>';
-										searchResultsHTML += '<div class="linkToTrailer"><a href="'+youtubeLink+'"><span class="glyphicon glyphicon-play"></span>&nbspPlay trailer</a>' + '</div><br>';	
-										searchResultsHTML += '<div class="release">Release Date: '+releaseDate+'</div><br>';
-										searchResultsHTML += '<div class="overview">' +overview+ '</div><br>';
-										searchResultsHTML += '<div class="rating">Rating: '+voteAverage+ '/10</div><br>';
-										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">8:30 AM' + '</div>';
-										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">10:00 AM' + '</div>';
-										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">12:30 PM' + '</div>';
-										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">3:00 PM' + '</div>';
-										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">4:10 PM' + '</div>';
-										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">5:30 PM' + '</div>';
-										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">8:00 PM' + '</div>';
-										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">10:30 PM' + '</div>';
+										// searchResultsHTML += '<div class="linkToTrailer"><a href="'+youtubeLink+'"><span class="glyphicon glyphicon-play"></span>&nbspPlay trailer</a>' + '</div><br>';	
+										// searchResultsHTML += '<div class="release">Release Date: '+releaseDate+'</div><br>';
+										// searchResultsHTML += '<div class="overview">' +overview+ '</div><br>';
+										// searchResultsHTML += '<div class="rating">Rating: '+voteAverage+ '/10</div><br>';
+										// searchResultsHTML += '<div class="col-sm-3 btn btn-primary">8:30 AM' + '</div>';
+										// searchResultsHTML += '<div class="col-sm-3 btn btn-primary">10:00 AM' + '</div>';
+										// searchResultsHTML += '<div class="col-sm-3 btn btn-primary">12:30 PM' + '</div>';
+										// searchResultsHTML += '<div class="col-sm-3 btn btn-primary">3:00 PM' + '</div>';
+										// searchResultsHTML += '<div class="col-sm-3 btn btn-primary">4:10 PM' + '</div>';
+										// searchResultsHTML += '<div class="col-sm-3 btn btn-primary">5:30 PM' + '</div>';
+										// searchResultsHTML += '<div class="col-sm-3 btn btn-primary">8:00 PM' + '</div>';
+										// searchResultsHTML += '<div class="col-sm-3 btn btn-primary">10:30 PM' + '</div>';
 									searchResultsHTML += '</div>'; //close movieDetails
 							searchResultsHTML += '</div>'; //close modal-dialog
 						searchResultsHTML += '</div>'; //close modal
